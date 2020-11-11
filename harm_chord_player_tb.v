9d431a886d8e697de9658544252010fe3b8707a6
a-saldana <asaldana@stanford.edu>
2020-11-10 22:51:42 -0800

    okok

--- harm_chord_player_tb.v
+++ harm_chord_player_tb.v
@@ -2,13 +2,19 @@ module harm_chord_player_tb();
 
 // Inputs
 reg clk, reset, play_enable, load_new_note, activate, generate_next_sample;
-wire beat;
 reg [1:0] weight;
 reg [5:0] note_to_load, duration;
 //Outputs
-wire sample_ready, note_done;
+wire sample_ready, note_done, beat;
 wire [17:0] final_sample;
 
+beat_generator #(.WIDTH(17), .STOP(5)) beat_generator(
+     .clk(clk),
+     .reset(reset),
+     .en(1'b1),
+     .beat(beat)
+);
+    
 harm_chord_player hcp1(
     .clk(clk),
     .reset(reset),
@@ -20,19 +26,11 @@ harm_chord_player hcp1(
     .beat(beat),
     .generate_next_sample(generate_next_sample),
     .weight(weight),
-	// outputs
+         // outputs
     .final_sample(final_sample),
     .note_done(note_done),
     .sample_ready(sample_ready)
 );
-
-beat_generator #(.WIDTH(17), .STOP(5)) beat_generator2(
-    .clk(clk),
-    .reset(reset),
-    .en(1'b1),
-    .beat(beat)
-);
-
 
     // Clock and reset
     initial begin
@@ -44,18 +42,16 @@ beat_generator #(.WIDTH(17), .STOP(5)) beat_generator2(
     end
 
     // Tests
-    //integer delay = 50000;
+   //integer delay = 50000;
     initial begin
         reset = 1'b0;
         #10
         reset = 1'b1;
         
         play_enable = 1'b1;
-
         weight = 2'd2;
-        generate_next_sample = 1'b1; // flip on and off??
         activate = 1'b0; // off until load all the notes
-
+        #30
         // Chord - load note 1, same durations
         duration = 6'd4;
         note_to_load = 6'd36;
@@ -74,16 +70,17 @@ beat_generator #(.WIDTH(17), .STOP(5)) beat_generator2(
         
         // Chord - load note 3, same durations
         duration = 6'd4;
-        note_to_load = 6'd30;
+        note_to_load = 6'd26;
         load_new_note = 1'b1;
-        #10
+        #20
         load_new_note = 1'b0;
         #10       
         
         // play chord, same durations
         activate = 1'b1;
+        generate_next_sample = 1'b1; // flip on and off??
         #5000
-        activate = 1'b0;
+        //activate = 1'b0;
     
         $stop;
     end
