4fc43b1786fcf9b5d8e96433b0638db5bf5aaa7a
curlymess <54120650+curlymess@users.noreply.github.com>
2020-11-10 21:47:29 -0800

    updates to harm chord player tb

--- harm_chord_player.v
+++ harm_chord_player.v
@@ -1,20 +1,19 @@
 module harm_chord_player(
     input  clk,
     input  reset,
-    input  play_enable,          // When high we play, when low we don't.
+    input  play_enable,         // When high we play, when low we don't.
     input  [5:0] note_to_load,  // The note to play
     input  [5:0] duration,      // The duration of the note to play
     input  load_new_note,       // Tells us when we have a new note to load
-    input  activate,             // Tells us if the counters should counting
-    input  beat,                 // This is our 1/48th second beat
-    input  generate_next_sample, // Tells us when the codec wants a new sample
+    input  activate,            // Tells us if the counters should counting
+    input  beat,                // This is our 1/48th second beat
+    input  generate_next_sample,// Tells us when the codec wants a new sample
+    input  [1:0] weight,
 	
     output [17:0] final_sample,  // Our sample output - note1,2,3 and harmonics together!
     output note_done,          // When we are done with a note this stays high - combo of note_done1,2,3
     output sample_ready     // Tells the codec when we've got a sample - combo of sample_ready1,2,3
 );
-
-/////////NOTE GET COUNT FROM NOTE PLAYERS! OR NOTE DONES?	
 
 /// Generate Samples for each Note    
 reg  [5:0] duration1, duration2, duration3;
--- harm_chord_player_tb.v
+++ harm_chord_player_tb.v
@@ -1,9 +1,29 @@
 module harm_chord_player_tb();
-    reg clk, reset, next_button, play_button;
-    wire new_frame;
-    wire [15:0] sample;
 
-    //instantiate harm_chord_player
+// Inputs
+reg clk, reset, play_enable, load_new_note, activate, beat, generate_next_sample;
+reg [1:0] weight;
+reg [5:0] note_to_load, duration;
+//Outputs
+wire sample_ready, note_done;
+wire [17:0] final_sample;
+
+harm_chord_player hcp1(
+    .clk(clk),
+    .reset(reset),
+    .play_enable(play_enable),    
+    .note_to_load(note_to_load),  
+    .duration(duration),
+    .load_new_note(load_new_note),
+    .activate(activate),
+    .beat(beat),
+    .generate_next_sample(generate_next_sample),
+    .weight(weight),
+	// outputs
+    .final_sample(final_sample),
+    .note_done(note_done),
+    .sample_ready(sample_ready)
+);
 
     // Clock and reset
     initial begin
@@ -15,11 +35,47 @@ module harm_chord_player_tb();
     end
 
     // Tests
-    integer delay;
+    //integer delay = 50000;
     initial begin
+        reset = 1'b0;
+        #10
+        reset = 1'b1;
+        
+        play_enable = 1'b1;
+        beat = 1'b1;
+        weight = 2'd2;
+        generate_next_sample = 1'b1; // flip on and off??
+        activate = 1'b0; // off until load all the notes
 
-
+        // Chord - load note 1, same durations
+        duration = 6'd4;
+        note_to_load = 6'd36;
+        load_new_note = 1'b1;
+        #10
+        load_new_note = 1'b0;
+        #10
+        
+        // Chord - load note 2, same durations
+        duration = 6'd4;
+        note_to_load = 6'd32;
+        load_new_note = 1'b1;
+        #10
+        load_new_note = 1'b0;
+        #10       
+        
+        // Chord - load note 3, same durations
+        duration = 6'd4;
+        note_to_load = 6'd30;
+        load_new_note = 1'b1;
+        #10
+        load_new_note = 1'b0;
+        #10       
+        
+        // play chord, same durations
+        activate = 1'b1;
+        #5000
+        activate = 1'b0;
+    
+        $stop;
     end
-
-
 endmodule
