2df735758d3d31333bbe112f1576432e899b70a0
curlymess <54120650+curlymess@users.noreply.github.com>
2020-11-18 21:21:07 -0800

    tcgrom

--- note_player.v
+++ note_player.v
@@ -4,8 +4,8 @@ module note_player(
     input  play_enable,         // When high we play, when low we don't.
     input  [5:0] note_to_load,  // The note to play
     input  load_new_note,       // Tells us when we have a new note to load
-	input [1:0] weight,	
-	input note_done,	        // Informs create_harmonic how the weight of each harmony
+	input  [1:0] weight,	
+	input  note_done,	        // Informs create_harmonic how the weight of each harmony
     input  generate_next_sample,// Tells us when the codec wants a new sample
     output [17:0] harmonic_out, // Our sample output
     output harmonic_ready     	// Tells the codec when we've got a sample
